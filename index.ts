7788bcfec38a919ba5f90182083c5233fe12389a
milanjrodd <milanrodd@gmail.com>
2024-02-02 16:10:28 +0300

    add encoder options

--- index.ts
+++ index.ts
@@ -1,10 +1,26 @@
 import { $, env } from "bun";
 
+const presets = {
+  ultrafast: "ultrafast",
+  superfast: "superfast",
+  veryfast: "veryfast",
+  faster: "faster",
+  fast: "fast",
+  medium: "medium",
+  slow: "slow",
+  slower: "slower",
+  veryslow: "veryslow",
+} as const;
+
 interface IEncoderOptions {
-  audioBitrate?: number;
-  videoBitrate?: number;
+  audioSamplingRate: number;
+  videoBitrate: number;
+  reconnectDelayInSeconds: number;
+  inputBufferSizeInKB: number;
   bufferSize?: number;
-  frameRate?: number;
+  frameRate: number;
+  preset: (typeof presets)[keyof typeof presets];
+  bframes: number;
 }
 
 async function SendSrtToRtmp(
@@ -12,17 +28,33 @@ async function SendSrtToRtmp(
   rtmpUrl: string,
   autoInstall = true,
   encoderOptions: IEncoderOptions = {
-    audioBitrate: 44100,
+    audioSamplingRate: 44100,
     videoBitrate: 8000,
-    bufferSize: 16000,
+    inputBufferSizeInKB: 1024,
+    reconnectDelayInSeconds: 2,
     frameRate: 48,
+    preset: "superfast",
+    bframes: 2,
   }
 ) {
   if (!srtUrl) throw new Error("SRT URL is required");
   if (!rtmpUrl) throw new Error("RTMP URL is required");
 
-  const ffmpeg =
-    await $`ffmpeg -i ${srtUrl} -ar ${encoderOptions.audioBitrate} -c:v libx264 -b:v ${encoderOptions.videoBitrate}k -bufsize ${encoderOptions.bufferSize}k  -r ${encoderOptions.frameRate} -f flv "${rtmpUrl}"`;
+  const ffmpeg = await $`ffmpeg -re -i ${
+    srtUrl +
+    "&recv_buffer_size=" +
+    encoderOptions.inputBufferSizeInKB * 1024 +
+    "&snddropdelay=" +
+    encoderOptions.reconnectDelayInSeconds * 1000 * 1000
+  } -ar ${
+    encoderOptions.audioSamplingRate
+  } -c:v libx264 -x264opts nal-hrd=cbr:bframes=${
+    encoderOptions.bframes
+  }:keyint=${2 * encoderOptions.frameRate}:no-scenecut -preset ${
+    encoderOptions.preset
+  } -c:a aac -b:a 160k -b:v ${encoderOptions.videoBitrate}k -bufsize ${
+    encoderOptions.bufferSize ?? encoderOptions.videoBitrate * 2
+  }k -filter:v fps=${encoderOptions.frameRate} -f flv "${rtmpUrl}"`;
 
   if (ffmpeg.stderr !== null) {
     const error = ffmpeg.stderr.toString();
